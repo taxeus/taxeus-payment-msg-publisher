00e25a28ad36b7fdaef28d28e34ab8f5206c4546
Ricardo Mendes <ricardolsmendes@gmail.com>
2021-03-01 00:16:12 -0300

    Remove the Firestore related stuff

--- index.js
+++ index.js
@@ -14,8 +14,7 @@
 
 'use strict';
 
-// Imports the Google Cloud client libraries
-const { Firestore } = require('@google-cloud/firestore');
+// Imports the Google Cloud client library.
 const { PubSub } = require('@google-cloud/pubsub');
 
 /**
@@ -23,26 +22,12 @@ const { PubSub } = require('@google-cloud/pubsub');
  *
  * @param {Object} req Cloud Function request context.
  */
-exports.storeAndPublishMessage = req => {
-  storeMessage(req.body);
-  publishMessage(req.body);
-};
-
-async function storeMessage(message) {
-  const db = new Firestore({ projectId: process.env.PROJECT_ID });
-
-  const collection = db.collection(process.env.FIRESTORE_COLLECTION_PATH);
-  const messageRef = collection.doc(`${message.resource.id}-${message.event}`);
-
-  await messageRef.set(message);
-  console.log(`Message ${message.resource.id}-${message.event} stored.`);
-}
-
-async function publishMessage(message) {
+exports.publishMessage = req => {
   const pubSubClient = new PubSub();
 
   const topic = pubSubClient.topic(process.env.PUBSUB_TOPIC_NAME);
 
+  const message = req.body;
   const messageId = await topic.publish(message);
   console.log(`Message ${message.resource.id}-${message.event} published with ID ${messageId}.`);
-}
+};
